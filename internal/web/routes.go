21844a090157553258bf07a5eaced6706cf2cfe9
amir daaee <amir.daaee@gmail.com>
2024-09-14 10:05:30 +0330

    auth in api group

--- internal/web/routes.go
+++ internal/web/routes.go
@@ -10,12 +10,12 @@ import (
 func SetupRoutes(r *gin.Engine, wp *bot.WorkerPool, mongo *db.Mongo, minio *db.MinioClient, streamChunckSize int64) {
 	r.GET("/stream/:mediaID", streamHandlerFactory(wp, mongo, streamChunckSize))
 	// ...
-	grpApi := r.Group("/api", tokenAuthMiddleware())
-	grpApi.GET("/media", listMediaHandlerFactory(mongo))
-	grpApi.DELETE("/media/:mediaID", deleteMediaHandlerFactory(wp, mongo, minio))
+	grpApi := r.Group("/api/media", tokenAuthMiddleware())
+	grpApi.GET("/", listMediaHandlerFactory(mongo))
+	grpApi.DELETE("/:mediaID", deleteMediaHandlerFactory(wp, mongo, minio))
 	// ...
 	cfg := config.Config()
-	grpAuth := r.Group("/auth")
+	grpAuth := r.Group("/api/auth")
 	grpAuth.POST("/login", loginHandlerFactory(cfg.UserName, cfg.UserPass, cfg.UserToken))
-	grpAuth.GET("/session", sessionHandlerFactory(cfg.UserToken), tokenAuthMiddleware())
+	grpAuth.GET("/session", tokenAuthMiddleware(), sessionHandlerFactory(cfg.UserToken))
 }
