b3780606b5c5de5abd2c53b4e729b1f7659308b6
amir daaee <amir.daaee@gmail.com>
2024-10-10 13:25:34 +0330

    gin access log to file

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"io"
+	"os"
+
 	"github.com/amirdaaee/TGMon/cmd"
 	"github.com/amirdaaee/TGMon/config"
 	"github.com/amirdaaee/TGMon/internal/web"
@@ -19,8 +22,20 @@ func main() {
 		logrus.WithError(err).Fatal("can not create minio client")
 	}
 	// ...
+	accLogFile := config.Config().AccessLogFile
+	if accLogFile != "" {
+		gin.DisableConsoleColor()
+		if f, err := os.Create(accLogFile); err != nil {
+			logrus.WithError(err).Fatalf("can not create access log file at %s", accLogFile)
+		} else {
+			gin.DefaultWriter = io.MultiWriter(f)
+			logrus.Infof("access log file at %s", accLogFile)
+		}
+	}
+	// ...
 	r := gin.Default()
 	web.SetupRoutes(r, wp, mongo, minio, config.Config().StreamChunkSize)
+	// ...
 	if err := r.Run(); err != nil {
 		logrus.WithError(err).Error("server terminated")
 	}
--- config/types.go
+++ config/types.go
@@ -8,17 +8,20 @@ type configType struct {
 	WorkerTokens    []string `env:"WORKER_TOKENS,notEmpty"`
 	SessionDir      string   `env:"SESSION_DIR" envDefault:"sessions"`
 	ChannelID       int64    `env:"CHANNEL_ID,required"`
-	MongoDBUri      string   `env:"MONGODB_URI,required"`
-	MongoDBName     string   `env:"MONGODB_DB_NAME,required"`
-	UserName        string   `env:"USER_NAME,required"`
-	UserPass        string   `env:"USER_PASS,required"`
-	UserToken       string   `env:"USER_TOKEN,required"`
-	LogLevel        string   `env:"LOG_LEVEL" envDefault:"warning"`
 	StreamChunkSize int64    `env:"STREAM_CHUNK_SIZE" envDefault:"1048576"`
+	LogLevel        string   `env:"LOG_LEVEL" envDefault:"warning"`
+	// ...
+	UserName      string `env:"USER_NAME,required"`
+	UserPass      string `env:"USER_PASS,required"`
+	UserToken     string `env:"USER_TOKEN,required"`
+	AccessLogFile string `env:"ACCESS_LOG_FILE"`
 	// ...
 	MinioEndpoint  string `env:"MINIO_ENDPOINT,required"`
 	MinioAccessKey string `env:"MINIO_ACCESS_KEY,required"`
 	MinioSecretKey string `env:"MINIO_SECRET_KEY,required"`
 	MinioBucket    string `env:"MINIO_BUCKET,required"`
 	MinioSecure    bool   `env:"MINIO_SECURE" envDefault:"true"`
+	// ...
+	MongoDBUri  string `env:"MONGODB_URI,required"`
+	MongoDBName string `env:"MONGODB_DB_NAME,required"`
 }
